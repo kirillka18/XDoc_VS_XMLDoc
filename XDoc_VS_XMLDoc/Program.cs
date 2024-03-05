637f46f57433c056e3e2cca317c0646f2f3bc306
Кирилл Пронин <samkirill10081998@gmail.com>
2024-03-05 22:24:46 +0300

    Добавил бенчи на удаление и добавление узлов. То что закомментил  - не отработало

--- XDoc_VS_XMLDoc/Program.cs
+++ XDoc_VS_XMLDoc/Program.cs
@@ -11,18 +11,24 @@ BenchmarkRunner.Run<XDoc_VS_XMLDoc>();
 //[MemoryDiagnoser]
 public class XDoc_VS_XMLDoc
 {
-    public string _filePath;
-    public byte[] _xmlByteArray;
+    private string _filePath;
+    private byte[] _xmlByteArray;
+    private XmlDocument _xmlDocBooks;
+    private XDocument _xDocBooks;
 
     [GlobalSetup]
     public void Setup()
     {
         _filePath = "C:\\Users\\Кирилл\\source\\repos\\XDoc_VS_XMLDoc\\XDoc_VS_XMLDoc\\XmlSamples\\books.xml";
-
         _xmlByteArray = File.ReadAllBytes(_filePath);
+        _xmlDocBooks = new();
+        _xmlDocBooks.Load(_filePath);
+        _xDocBooks = XDocument.Load(_filePath);
+
     }
 
-    // Простейшие тесты загрузки/чтения/изменения с файлом books.xml
+    //// Простейшие тесты с файлом books.xml
+    //// Подгрузка из байтового массива/файла ods в XDoc/XMLDoc
     [Benchmark]
     public XmlDocument LoadXml_XmlDocument_Load()
     {
@@ -58,6 +64,66 @@ public class XDoc_VS_XMLDoc
 
         return doc;
     }
+
+    // Добавление, удаление xml в XDoc/XMLDoc
+    //[Benchmark]
+    //public void AppendChildNode_XmlDocument()
+    //{
+    //    // Создание нового элемента
+    //    XmlElement newElement = _xmlDocBooks.CreateElement("book");
+
+    //    // Добавление атрибута к элементу
+    //    newElement.SetAttribute("category", "fiction");
+
+    //    // Создание и добавление дочерних элементов
+    //    XmlElement titleElement = _xmlDocBooks.CreateElement("title");
+    //    titleElement.InnerText = "The Great Gatsby";
+    //    newElement.AppendChild(titleElement);
+
+    //    XmlElement authorElement = _xmlDocBooks.CreateElement("author");
+    //    authorElement.InnerText = "F. Scott Fitzgerald";
+    //    newElement.AppendChild(authorElement);
+
+    //    // Добавление нового элемента в корневой элемент документа
+    //    _xmlDocBooks.DocumentElement.AppendChild(newElement);
+    //}
+
+    //[Benchmark]
+    //public void AppendChildNode_XDocument()
+    //{
+    //    // Создание нового элемента
+    //    XElement newElement = new XElement("book",
+    //        new XAttribute("category", "fiction"),
+    //        new XElement("title", "The Great Gatsby"),
+    //        new XElement("author", "F. Scott Fitzgerald")
+    //    );
+
+    //    // Добавление нового элемента в корневой элемент документа
+    //    _xDocBooks.Root.Add(newElement);
+    //}
+
+    //[Benchmark]
+    //public void RemoveChildNode_XmlDocument()
+    //{
+    //    // Нахождение узла, который нужно удалить
+    //    XmlNode nodeToRemove = _xmlDocBooks.SelectSingleNode("catalog").SelectSingleNode("//book[@id='bk102']");
+
+    //    // Удаление узла из документа
+    //    nodeToRemove.ParentNode.RemoveChild(nodeToRemove);
+    //}
+
+    [Benchmark]
+    public void RemoveChildNode_XDocument()
+    {
+        // Нахождение узла, который нужно удалить
+        XElement nodeToRemove = _xDocBooks.Element("catalog")
+                                      .Elements("book")
+                                      .FirstOrDefault(b => (string)b.Attribute("id") == "bk102");
+
+        // Удаление узла из документа
+        nodeToRemove?.Remove();
+    }
+
 
     // Взаимодействие с OpenOffice Document (обычная таблица)
     // Подгрузка из файла ods в XDoc/XMLDoc
