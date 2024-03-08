f11a70e4d65eb1588c92513d595c7f6b6eb9e336
Кирилл Пронин <samkirill10081998@gmail.com>
2024-03-08 13:50:45 +0300

    Добавил разграничение Бенчей на 3 вида. Встроил детальное опивание и починил логику.

---
+++ XDoc_VS_XMLDoc/Benches/BenchesOnKladr.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XDoc_VS_XMLDoc.Benches;
+
+//[MemoryDiagnoser]
+public class BenchesOnKladr
+{
+    private string _filePath;
+    private byte[] _xmlByteArray;
+    private XmlDocument _xmlDocBooks;
+    private XDocument _xDocBooks;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _filePath = "C:\\Users\\Кирилл\\source\\repos\\XDoc_VS_XMLDoc\\XDoc_VS_XMLDoc\\XmlSamples\\books.xml";
+        _xmlByteArray = File.ReadAllBytes(_filePath);
+        _xmlDocBooks = new();
+        _xmlDocBooks.Load(_filePath);
+        _xDocBooks = XDocument.Load(_filePath);
+    }
+
+    // Взаимодействие с КЛАДР XML (большая таблица)
+    // Подгрузка из файла ods в XDoc/XMLDoc
+    // Чтение, запись, удаление xml в XDoc/XMLDoc
+}
