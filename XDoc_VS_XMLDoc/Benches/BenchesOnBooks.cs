949344a0dce1017df7ee71fa097b1aceb585c74e
Кирилл Пронин <samkirill10081998@gmail.com>
2024-03-19 20:22:46 +0300

    Вставил правильные данные для подгрузки xml файла во все тесты.

--- XDoc_VS_XMLDoc/Benches/BenchesOnBooks.cs
+++ XDoc_VS_XMLDoc/Benches/BenchesOnBooks.cs
@@ -4,22 +4,17 @@ using System.Xml.Linq;
 
 namespace XDoc_VS_XMLDoc.Benches;
 
-//[MemoryDiagnoser]
+[MemoryDiagnoser]
 public class BenchesOnBooks
 {
     private string _filePath;
     private byte[] _xmlByteArray;
-    private XmlDocument _xmlDocBooks;
-    private XDocument _xDocBooks;
 
     [GlobalSetup]
     public void Setup()
     {
         _filePath = "C:\\Users\\Кирилл\\source\\repos\\XDoc_VS_XMLDoc\\XDoc_VS_XMLDoc\\XmlSamples\\books.xml";
         _xmlByteArray = File.ReadAllBytes(_filePath);
-        _xmlDocBooks = new();
-        _xmlDocBooks.Load(_filePath);
-        _xDocBooks = XDocument.Load(_filePath);
     }
 
     //// Простейшие тесты с файлом books.xml
@@ -64,28 +59,37 @@ public class BenchesOnBooks
     [Benchmark]
     public void AppendChildNode_XmlDocument()
     {
+        // Берем байтовый массив файла books.xml и создаем doc для правильного теста
+        XmlDocument doc = new XmlDocument();
+        using MemoryStream ms = new MemoryStream(_xmlByteArray);
+        doc.Load(ms);
+
         // Создание нового элемента
-        XmlElement newElement = _xmlDocBooks.CreateElement("book");
+        XmlElement newElement = doc.CreateElement("book");
 
         // Добавление атрибута к элементу
         newElement.SetAttribute("category", "fiction");
 
         // Создание и добавление дочерних элементов
-        XmlElement titleElement = _xmlDocBooks.CreateElement("title");
+        XmlElement titleElement = doc.CreateElement("title");
         titleElement.InnerText = "The Great Gatsby";
         newElement.AppendChild(titleElement);
 
-        XmlElement authorElement = _xmlDocBooks.CreateElement("author");
+        XmlElement authorElement = doc.CreateElement("author");
         authorElement.InnerText = "F. Scott Fitzgerald";
         newElement.AppendChild(authorElement);
 
         // Добавление нового элемента в корневой элемент документа
-        _xmlDocBooks.DocumentElement.AppendChild(newElement);
+        doc.DocumentElement.AppendChild(newElement);
     }
 
     [Benchmark]
     public void AppendChildNode_XDocument()
     {
+        // Берем байтовый массив файла books.xml и создаем doc для правильного теста
+        using MemoryStream ms = new MemoryStream(_xmlByteArray);
+        XDocument doc = XDocument.Load(ms);
+
         // Создание нового элемента
         XElement newElement = new XElement("book",
             new XAttribute("category", "fiction"),
@@ -94,14 +98,19 @@ public class BenchesOnBooks
         );
 
         // Добавление нового элемента в корневой элемент документа
-        _xDocBooks.Root.Add(newElement);
+        doc.Root.Add(newElement);
     }
 
     [Benchmark]
     public void RemoveChildNode_XmlDocument()
     {
+        // Берем байтовый массив файла books.xml и создаем doc для правильного теста
+        XmlDocument doc = new XmlDocument();
+        using MemoryStream ms = new MemoryStream(_xmlByteArray);
+        doc.Load(ms);
+
         // Нахождение узла, который нужно удалить
-        XmlNode nodeToRemove = _xmlDocBooks.SelectSingleNode("catalog").SelectSingleNode("//book[@id='bk102']");
+        XmlNode nodeToRemove = doc.SelectSingleNode("catalog").SelectSingleNode("//book[@id='bk102']");
 
         // Удаление узла из документа
         nodeToRemove.ParentNode.RemoveChild(nodeToRemove);
@@ -110,10 +119,14 @@ public class BenchesOnBooks
     [Benchmark]
     public void RemoveChildNode_XDocument()
     {
+        // Берем байтовый массив файла books.xml и создаем doc для правильного теста
+        using MemoryStream ms = new MemoryStream(_xmlByteArray);
+        XDocument doc = XDocument.Load(ms);
+
         // Нахождение узла, который нужно удалить
-        XElement nodeToRemove = _xDocBooks.Element("catalog")
-                                      .Elements("book")
-                                      .FirstOrDefault(b => (string)b.Attribute("id") == "bk102");
+        XElement nodeToRemove = doc.Element("catalog")
+                                   .Elements("book")
+                                   .FirstOrDefault(b => (string)b.Attribute("id") == "bk102");
 
         // Удаление узла из документа
         nodeToRemove?.Remove();
